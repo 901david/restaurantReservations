8f0422206c1549e6d9e3ff855520f4eea0977101
DHLAP\h <vdavidhammond@gmail.com>
2017-08-05 11:49:23 -0600

    bad bing bada boom

--- js/app.js
+++ js/app.js
@@ -8,9 +8,47 @@ var path = require("path");
 // =============================================================
 var app = express();
 var PORT = 3000;
+var uniqueId = 1;
 
+var tables = [];
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "../index.html"));
+});
+app.get("/tables", function(req, res) {
+  res.sendFile(path.join(__dirname, "../view.html"));
+});
+app.get("/reserve", function(req, res) {
+  res.sendFile(path.join(__dirname, "../reserve.html"));
+});
+app.get("/api/tables", function(req, res) {
+  res.json(currentReservations);
+});
+app.get("/api/list", function(req, res) {
+  res.json(currentWait);
+});
+
+var currentReservations = [{name: "Chris", phone: "3032960017", email:  "fake@fake.com"}];
+var currentWait = [{name: "Mike", phone: "3032960017", email:  "fake@fake.com"}];
+
+  app.post("/api/new", function(req, res) {
+    var newReservation = req.body;
+    // newReservation.routeName = newReservation.name;
+
+    // console.log(newReservation);
+
+    currentReservations.push(newReservation);
+    //
+    res.json(currentReservations);
+  });
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
---
+++ js/tables.js
@@ -0,0 +1,1 @@
+{name: "Chris", phone: "303-296-0017", email: "fake@fake.com", id: "001"}];
